76674a8a0706485ae2d4f82c4dc33bafad0ad271
taichow <w.lafdili@gmail.com>
2019-05-30 16:33:20 +0200

    corrected layout

--- resources/views/layout/blog-layout.blade.php
+++ resources/views/layout/blog-layout.blade.php
@@ -19,22 +19,7 @@
     </head>
     <body>
 
-        @include('parts.header')
-
-        {{-- @if ($errors->any())
-          <div class="container">
-
-
-          @foreach ($errors->all() as $error)
-            <div class="alert alert-danger" role="alert">
-              {{ $error }}
-            </div>
-
-          @endforeach --}}
-
-
-          </div>
-        @endif
+        @include('parts.header')       
 
         @yield('content')
 
